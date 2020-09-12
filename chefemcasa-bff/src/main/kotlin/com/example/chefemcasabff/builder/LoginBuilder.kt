9ab01a4733bf5a2b8073e09e66b8f5fc5e312bab
heliohachimine <heliohachimine@gmail.com>
2020-09-12 19:46:38 -0300

    remove idea files

--- chefemcasa-bff/src/main/kotlin/com/example/chefemcasabff/builder/LoginBuilder.kt
+++ chefemcasa-bff/src/main/kotlin/com/example/chefemcasabff/builder/LoginBuilder.kt
@@ -1,4 +1,12 @@
 package com.example.chefemcasabff.builder
 
-class LoginBuilder {
+import br.com.zup.beagle.widget.layout.Screen
+import br.com.zup.beagle.widget.layout.ScreenBuilder
+
+class LoginBuilder : ScreenBuilder {
+
+    override fun build(): Screen {
+        TODO("Not yet implemented")
+    }
+
 }
