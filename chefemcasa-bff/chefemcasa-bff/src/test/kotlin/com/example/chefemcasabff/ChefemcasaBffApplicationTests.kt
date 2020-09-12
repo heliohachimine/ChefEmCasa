113edc00391208eaa72ae8982b587a56a55e0a7e
heliohachimine <heliohachimine@gmail.com>
2020-09-12 18:57:30 -0300

    first commit

---
+++ chefemcasa-bff/chefemcasa-bff/src/test/kotlin/com/example/chefemcasabff/ChefemcasaBffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chefemcasabff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChefemcasaBffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
