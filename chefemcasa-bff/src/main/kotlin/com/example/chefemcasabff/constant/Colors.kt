3e401a624f60c3e4b66b7ad08c17883a0dd44a75
heliohachimine <heliohachimine@gmail.com>
2020-09-12 20:41:40 -0300

    login builder

---
+++ chefemcasa-bff/src/main/kotlin/com/example/chefemcasabff/constant/Colors.kt
@@ -0,0 +1,9 @@
+package com.example.chefemcasabff.constant
+
+const val BLACK = "#000000"
+const val DARK_SIENNA = "#461220"
+const val RED_VIOLET_COLOR_WHEEL = "#8C2F39"
+const val ENGLISH_RED = "#B23A48"
+const val MELON = "#FCB9B2"
+const val APRICOT = "#FED0BB"
+const val WHITE = "#FFFFFF"
